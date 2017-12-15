27cdebb16b42071577a66cf9d6681068bcd23dde
tianyi7654 <250642598@qq.com>
2017-12-15 17:51:27 +0800

    修改网络基类2

--- OpenSourceDemo/opensourcelibrary/src/main/java/com/inca/administrator/opensourcelibrary/http/TFNetCallBack.java
+++ OpenSourceDemo/opensourcelibrary/src/main/java/com/inca/administrator/opensourcelibrary/http/TFNetCallBack.java
@@ -3,16 +3,24 @@ package com.inca.administrator.opensourcelibrary.http;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.apache.http.Header;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public abstract class TFNetCallBack extends JsonHttpResponseHandler {
 
 	@Override
-	public abstract void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse);
+	public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+		onSuccess(statusCode,response);
+	}
 
 	@Override
-	public abstract void onSuccess(int statusCode, Header[] headers,JSONObject response);
+	public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+	onFailure(statusCode,throwable,errorResponse);
+	}
 
-	
+	public abstract void onFailure(int statusCode, Throwable throwable, JSONObject errorResponse);
+
+
+	public abstract void onSuccess(int statusCode,JSONObject response);
 
 }
